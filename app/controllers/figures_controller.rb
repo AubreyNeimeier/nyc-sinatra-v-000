9bcdfb323acbfbfddf1e452b6042313f5ebd6dbf
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-06 19:13:51 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,17 +15,45 @@ class FiguresController < ApplicationController
 
   get '/figures/:id' do
     #render show page
+    #display new figure and its attributes (figure name, landmarks, and titles)
+
     @figure = Figure.find_by_id(params[:id])
-    @landmarks = @figure.landmarks
-    #binding.pry
     erb :"figures/show"
   end
 
   post '/figures' do
-    #binding.pry
-    #display new figure and its attributes (figure name, landmarks, and titles)
-    #reroute to '/figures/:id'
+    #create a new figure
+    @figure = Figure.create(name: params[:figure][:name])
+    #create a new landmark if they filled in the field
+    #shovel that landmark's id into params[landmark_ids]
+    if !!params[:landmark][:name].empty?
+        @landmark = Landmark.create(name: params[:landmark][:name])
+        params[:figure][:landmark_ids] << @landmark.id #too low level?
+        # @figure.landmarks << @landmark
+    #if the landmark field is blank, then simply assign the ids
+    else
+      @figure.landmark_ids = params[landmark_ids]
+    end
+
+
+    #create a new landmark if they filled in the field
+    #shovel that landmark's id into params[landmark_ids]
+    if !!params[:landmark][:name].empty?
+        @landmark = Landmark.create(name: params[:landmark][:name])
+        params[:figure][:landmark_ids] << @landmark.id #too low level?
+        # @figure.landmarks << @landmark
+    #if the landmark field is blank, then simply assign the ids
+    else
+      @figure.landmark_ids = params[landmark_ids]
+    end
+
+
+
+    @figure.landmarks =
+
+    reroute to "/figures/:id"
   end
+
 
   get '/figures/:id/edit' do
     #display the figure edit form
